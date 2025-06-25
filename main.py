c4ce73dc729425851cd04f25e488cd8d286e19f9
Evan <eg396@njit.edu>
2025-06-25 15:31:26 -0400

    chore: add main and pytest files

---
+++ main.py
@@ -0,0 +1,11 @@
+## main.py
+## IS 601 Module 5
+## Evan Garvey
+
+## This file runs the calculator
+
+from app.calculator_repl import calculator_repl ##TODO: not implemented yet. expect an error when running!
+
+
+if __name__ == "__main__":
+    calculator_repl()
